dcb9c21b93afe804c1e66b92d033678ee5d0d9ed
Imtiyaz Hoque Hazari <imtiyaz.hoque@brainstation-23.com>
2023-11-14 19:00:23 +0600

    lazy loading introduced, section 10 done

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -26,6 +26,11 @@ export class ShopService {
     return this.http.get<Pagination<Product[]>>(this.baseUrl+'products?pageSie=50');
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
   }
