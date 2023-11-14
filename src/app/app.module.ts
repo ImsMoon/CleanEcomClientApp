e73aa0bb60d690e61e529cb8757f81ad75f54191
Imtiyaz Hoque Hazari <imtiyaz.hoque@brainstation-23.com>
2023-11-14 17:17:01 +0600

    folder refactored and end of section 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
-import { ProductComponent } from './Modules/Products/Components/product/product.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    ProductComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shop/shop.component.ts
@@ -0,0 +1,77 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ShopService } from './shop.service';
+import { Product } from '../shared/Models/product';
+import { ShopParams } from '../shared/Models/shop-params';
+import { Brand } from '../shared/Models/brand';
+import { Type } from '../shared/Models/type';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search') searchTerm?:ElementRef
+  products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
+  shopParams = new ShopParams();
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to high', value: 'priceAsc'},
+    {name: 'Price: High to low', value: 'priceDesc'},
+  ];
+  totalCount = 0;
+  constructor(private service:ShopService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+
+  getProducts() {
+    this.service.getProducts(this.shopParams).subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+        console.log('extra statment');
+      }
+    });
+  }
+
+  onBrandSelected(brandId:number){
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onSortSelected(event:any){
+    this.shopParams.sort = event.target.value;
+    this.getProducts();
+  }
+
+  onPageChanged(event: any) {
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch(){
+    this.shopParams.search = this.searchTerm?.nativeElement.value;
+    this.getProducts();
+  }
+
+  onReset(){
+    if(this.searchTerm) this.searchTerm.nativeElement.value = '';
+    this.shopParams = new ShopParams();
+    this.getProducts();
+  }
+}
