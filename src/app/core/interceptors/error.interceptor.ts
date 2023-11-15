85dd7f49b6eb6b6ceffd8bf551a0b0043f3442d9
Imtiyaz Hoque Hazari <imtiyaz.hoque@brainstation-23.com>
2023-11-15 12:46:30 +0600

    error interceptor with error component and end of section 11

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { Toast, ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router:Router, private toastr:ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err:HttpErrorResponse )=>{
+        if(err){
+          if(err.status === 400){
+            if(err.error.errors){
+              throw err.error;
+            }else{
+              this.toastr.error(err.error.message, err.status.toString());
+            }
+          }
+          if(err.status === 401){
+            this.toastr.error(err.error.message, err.status.toString());
+          }
+          if(err.status === 404){
+              this.router.navigateByUrl('/not-found');
+          }
+          if(err.status === 500){
+            const navigationExtras: NavigationExtras = {state:{error:err.error}};
+            this.router.navigateByUrl('/server-error',navigationExtras);
+          }
+        }
+        return throwError(()=> new Error(err.message));
+      })
+    );
+  }
+}
+ 
